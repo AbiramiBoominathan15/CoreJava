e77531d103e5e7b86fbba9ec1f4d4e9dbb3f97e0
AbiramiBoominathan15 <165891244+AbiramiBoominathan15@users.noreply.github.com>
2024-05-07 17:42:34 +0530

    sql

---
+++ StockProject.sql
@@ -0,0 +1,11 @@
+show databases;
+create database test_db_7;
+use  test_db_7;
+create table StockProject(
+customerId varchar(255),
+regularCustomer varchar(255),
+sectionName varchar(255),
+username varchar(255),
+password varchar(200));
+select * from StockProject;
+drop database test_db_7;
